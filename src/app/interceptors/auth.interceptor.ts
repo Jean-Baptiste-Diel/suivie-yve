dbb22b31ff8b1e6a3de89af369fe4a722e9b6fcd
Jean Baptiste <jeanbaptistedielinang@gmail.com>
2025-11-25 00:15:57 +0100

    v4

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,27 @@
+import {inject} from '@angular/core';
+import { HttpInterceptorFn } from '@angular/common/http';
+import {AuthService} from '../core/auth.service';
+
+export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+  const token = authService.getToken();
+
+  // Ne pas toucher aux requêtes de connexion
+  if (req.url.includes('/connexion/')) {
+    return next(req);
+  }
+
+  let headersConfig: any = {};
+
+  if (token) {
+    headersConfig['Authorization'] = `Bearer ${token}`;
+  }
+
+  // Ajouter Content-Type JSON uniquement si ce n'est pas FormData
+  if (!(req.body instanceof FormData)) {
+    headersConfig['Content-Type'] = 'application/json';
+  }
+
+  const cloned = req.clone({ setHeaders: headersConfig });
+  return next(cloned);
+};
